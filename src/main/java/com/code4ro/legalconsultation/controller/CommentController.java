b8a40aef70cce2725a2dc18784ee7165351138f2
Lungu Daniel <lungu.daniel94@gmail.com>
2020-04-21 20:14:27 +0300

    Add swagger in CommentController (#110)

--- src/main/java/com/code4ro/legalconsultation/controller/CommentController.java
+++ src/main/java/com/code4ro/legalconsultation/controller/CommentController.java
@@ -3,58 +3,82 @@ package com.code4ro.legalconsultation.controller;
 import com.code4ro.legalconsultation.model.dto.CommentDto;
 import com.code4ro.legalconsultation.model.dto.CommentIdentificationDto;
 import com.code4ro.legalconsultation.service.api.CommentService;
-import lombok.AllArgsConstructor;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/api/documentnodes/{nodeId}/comments")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
+    @ApiOperation(value = "Create a new comment in the platform",
+            response = CommentDto.class,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping
-    public ResponseEntity<CommentDto> create(@PathVariable final UUID nodeId,
-                                             @RequestBody final CommentDto commentDto) {
+    public ResponseEntity<CommentDto> create(@ApiParam(value = "The id of the node") @PathVariable final UUID nodeId,
+                                             @ApiParam("The DTO object containing a new comment") @RequestBody final CommentDto commentDto) {
         return ResponseEntity.ok(commentService.create(nodeId, commentDto));
     }
 
+    @ApiOperation(value = "Update a comment in the platform",
+            response = CommentDto.class,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     @PutMapping("/{id}")
-    public ResponseEntity<CommentDto> update(@PathVariable final UUID nodeId,
-                                             @PathVariable final UUID id,
-                                             @RequestBody final CommentDto commentDto) {
-
+    public ResponseEntity<CommentDto> update(@ApiParam(value = "The id of the node") @PathVariable final UUID nodeId,
+                                             @ApiParam(value = "The id of the comment") @PathVariable final UUID id,
+                                             @ApiParam("The DTO object to update") @RequestBody final CommentDto commentDto) {
         return ResponseEntity.ok(commentService.update(nodeId, id, commentDto));
     }
 
+    @ApiOperation(value = "Delete a comment",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable final UUID nodeId,
-                                       @PathVariable final UUID id) {
+    public ResponseEntity<Void> delete(@ApiParam(value = "The id of the comment") @PathVariable final UUID id) {
         commentService.delete(id);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Get all comments of a node",
+            response = Page.class,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     @GetMapping
-    public ResponseEntity<Page<CommentIdentificationDto>> findAll(@PathVariable UUID nodeId,
-                                                                  final Pageable pageable) {
+    public ResponseEntity<Page<CommentIdentificationDto>> findAll(@ApiParam(value = "The id of the node") @PathVariable final UUID nodeId,
+                                                                  @ApiParam("Page object information being requested") final Pageable pageable) {
         return ResponseEntity.ok(commentService.findAll(nodeId, pageable));
     }
 
+    @ApiOperation(value = "Get all replies of a comment",
+            response = Page.class,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     @GetMapping("/{commentId}/replies")
-    public ResponseEntity<Page<CommentIdentificationDto>> findAllReplies(@PathVariable UUID commentId,
-                                                                         Pageable pageable) {
+    public ResponseEntity<Page<CommentIdentificationDto>> findAllReplies(@ApiParam(value = "The id of the comment") @PathVariable final UUID commentId,
+                                                                         @ApiParam("Page object information being requested") final Pageable pageable) {
         return ResponseEntity.ok(commentService.findAllReplies(commentId, pageable));
     }
 
+    @ApiOperation(value = "Create a new reply in the platform",
+            response = CommentDto.class,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping("/{commentId}/replies")
-    public ResponseEntity<CommentDto> createReply(@PathVariable UUID commentId,
-                                                  @RequestBody CommentDto commentDto) {
+    public ResponseEntity<CommentDto> createReply(@ApiParam(value = "The id of the comment") @PathVariable final UUID commentId,
+                                                  @ApiParam("The DTO object containing a new reply") @RequestBody CommentDto commentDto) {
         return ResponseEntity.ok(commentService.createReply(commentId, commentDto));
     }
 }
