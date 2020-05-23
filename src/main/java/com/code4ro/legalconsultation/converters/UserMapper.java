55952b0c471aabdfae902fbe28d75cec0173a3f6
cosmingodeanu <cgodeanu@mail.com>
2020-05-23 17:00:34 +0300

    Added user filtering

--- src/main/java/com/code4ro/legalconsultation/converters/UserMapper.java
+++ src/main/java/com/code4ro/legalconsultation/converters/UserMapper.java
@@ -4,9 +4,13 @@ import com.code4ro.legalconsultation.model.dto.UserDto;
 import com.code4ro.legalconsultation.model.persistence.User;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User map(UserDto userDto);
 
     UserDto map(User User);
+
+    List<UserDto> map(List<User> users);
 }
